c52c9f33a42f1db019bd7dcccd002a23ce9bbc54
hahahazel <hazel.h.wat@gmail.com>
2021-12-13 12:01:28 +0100

    streamlit tryout

--- WorkFolder_Hazel/barchart1.py
+++ WorkFolder_Hazel/barchart1.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+# 50 rows and 3 columns Pandas DataFrame
 chart_data = pd.DataFrame(
      np.random.randn(50, 3),
      columns=["a", "b", "c"])
